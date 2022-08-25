c3010d20afd3f029d547cf262aeaea706d7aac84
punia.umesh@gmail.com <punia.umesh@gmail.com>
2022-08-25 17:27:52 +0530

    routing

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -19,16 +19,14 @@ router.post("/register", (req, res) => {
     if (err) return res.status(500).json({ msg: err.message });
     if (!hashPass) return res.status(501).json({ msg: "Please Try Again" });
     // insert user into database
-    let insUser = new UserSchema({ email, password:hashPass });
-  
+    let insUser = new UserSchema({ email, password: hashPass });
+
     insUser.save((err, data) => {
       if (err) return res.status(500).json({ msg: err.message });
       if (!data) return res.status(501).json({ msg: "Dont Know" });
       res.status(200).json({ msg: "Done" });
     });
-
   });
-
 });
 
 router.get("/users", (req, res) => {
@@ -39,11 +37,42 @@ router.get("/users", (req, res) => {
   });
 });
 
+// get single user
+router.get("/:_id", (req, res) => {
+  const { _id } = req.params;
+  UserSchema.findOne({ _id }, (err, data) => {
+    if (err) return res.status(500).json({ msg: err.message });
+    if (!data) return res.status(200).json({ msg: "No User Found" });
+    res.status(200).json({ msg: data });
+  });
+});
+
+// update user
+router.put("/:_id", (req, res) => {
+  const { _id } = req.params;
+
+  const {email}=req.body
+
+  if(!email) return res.status(400).json({ msg: "Please Enter Details" });
+
+  UserSchema.findByIdAndUpdate({ _id }, {email},(err, data) => {
+    if (err) return res.status(500).json({ msg: err.message });
+    if (!data) return res.status(200).json({ msg: "No User Found" });
+    res.status(200).json({ msg: "User Updated" });
+  });
+});
 
 
 
+// delete user
+router.delete("/:_id", (req, res) => {
+  const { _id } = req.params;
 
-
-
+  UserSchema.findByIdAndDelete({ _id }, (err, data) => {
+    if (err) return res.status(500).json({ msg: err.message });
+    if (!data) return res.status(200).json({ msg: "No User Found" });
+    res.status(200).json({ msg: "User Deleted" });
+  });
+});
 
 module.exports = router;
